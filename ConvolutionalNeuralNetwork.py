cd509822024a15f226c0834b16a7f6f6748ee753
Jly_wave <597100700@qq.com>
2017-12-21 23:24:20 +0800

    add file

---
+++ ConvolutionalNeuralNetwork.py
@@ -0,0 +1,117 @@
+import pandas as pd
+import numpy as np
+from keras.models import Sequential
+from keras.layers import Dense, Dropout, Flatten, Activation
+from keras.layers import Conv2D, MaxPooling2D
+from keras.callbacks import EarlyStopping, ModelCheckpoint, ReduceLROnPlateau
+from keras.layers.normalization import BatchNormalization
+from keras.optimizers import Adam
+
+def get_scaled_imgs(df):
+    imgs = []
+
+    for i, row in df.iterrows():
+        #make 75x75 image
+        band_1 = np.array(row['band_1']).reshape(75, 75)
+        band_2 = np.array(row['band_2']).reshape(75, 75)
+        band_3 = band_1 + band_2 # plus since log(x*y) = log(x) + log(y)
+
+        # Rescale
+        a = (band_1 - band_1.mean()) / (band_1.max() - band_1.min())
+        b = (band_2 - band_2.mean()) / (band_2.max() - band_2.min())
+        c = (band_3 - band_3.mean()) / (band_3.max() - band_3.min())
+
+        imgs.append(np.dstack((a, b, c)))
+
+    return np.array(imgs)
+
+
+def getModel():
+    #Build keras model
+
+    model=Sequential()
+
+    # CNN 1
+    model.add(Conv2D(64, kernel_size=(3, 3),activation='relu', input_shape=(75, 75, 3)))
+    model.add(MaxPooling2D(pool_size=(3, 3), strides=(2, 2)))
+    model.add(Dropout(0.2))
+
+    # CNN 2
+    model.add(Conv2D(128, kernel_size=(3, 3), activation='relu' ))
+    model.add(MaxPooling2D(pool_size=(2, 2), strides=(2, 2)))
+    model.add(Dropout(0.2))
+
+    # CNN 3
+    model.add(Conv2D(128, kernel_size=(3, 3), activation='relu'))
+    model.add(MaxPooling2D(pool_size=(2, 2), strides=(2, 2)))
+    model.add(Dropout(0.2))
+
+    #CNN 4
+    model.add(Conv2D(64, kernel_size=(3, 3), activation='relu'))
+    model.add(MaxPooling2D(pool_size=(2, 2), strides=(2, 2)))
+    model.add(Dropout(0.2))
+
+    # You must flatten the data for the dense layers
+    model.add(Flatten())
+
+    #Dense 1
+    model.add(Dense(512, activation='relu'))
+    model.add(Dropout(0.2))
+
+    #Dense 2
+    model.add(Dense(256, activation='relu'))
+    model.add(Dropout(0.2))
+
+    # Output
+    model.add(Dense(1, activation="sigmoid"))
+
+    optimizer = Adam(lr=0.001, decay=0.0)
+    model.compile(loss='binary_crossentropy', optimizer=optimizer, metrics=['accuracy'])
+
+    return model
+
+df_train = pd.read_json('./input/train.json')
+Xtrain = get_scaled_imgs(df_train)
+Ytrain = np.array(df_train['is_iceberg'])
+
+df_train.inc_angle = df_train.inc_angle.replace('na',0)
+idx_tr = np.where(df_train.inc_angle>0)
+
+Ytrain = Ytrain[idx_tr[0]]
+Xtrain = Xtrain[idx_tr[0],...]
+
+# Xtrain = get_more_images(Xtrain)
+# Ytrain = np.concatenate((Ytrain,Ytrain,Ytrain))
+
+
+model = getModel()
+model.summary()
+
+batch_size = 32
+earlyStopping = EarlyStopping(monitor='val_loss', patience=10, verbose=0, mode='min')
+mcp_save = ModelCheckpoint('.mdl_wts.hdf5', save_best_only=True, monitor='val_loss', mode='min')
+reduce_lr_loss = ReduceLROnPlateau(monitor='val_loss', factor=0.1, patience=7, verbose=1, epsilon=1e-4, mode='min')
+
+model.fit(Xtrain, Ytrain, batch_size=batch_size, epochs=50, verbose=1,
+          callbacks=[earlyStopping, mcp_save, reduce_lr_loss], validation_split=0.25)
+
+
+model.load_weights(filepath = '.mdl_wts.hdf5')
+score = model.evaluate(Xtrain, Ytrain, verbose=1)
+print('Train score:', score[0])
+print('Train accuracy:', score[1])
+
+df_test = pd.read_json('./input/test.json')
+df_test.inc_angle = df_test.inc_angle.replace('na',0)
+Xtest = (get_scaled_imgs(df_test))
+pred_test = model.predict(Xtest)
+
+submission = pd.DataFrame({'id': df_test["id"], 'is_iceberg': pred_test.reshape((pred_test.shape[0]))})
+print(submission.head(10))
+
+submission.to_csv('output/cnn.csv', index=False)
+
+
+
+
+
